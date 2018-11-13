938f1c3ece5a12c3beadea6adf5ef7004b083b9f
Cohen <dan.cohen@emc.com>
2018-11-13 14:48:40 -0700

    fix port

--- 00-mlb-datafresh.R
+++ 00-mlb-datafresh.R
@@ -7,7 +7,7 @@ cat("R program running")
 names(s <- Sys.getenv())
 #print(Sys.getenv("mlb_db_hostname"))
 
-my_db <- src_mysql(dbname = Sys.getenv("mlb_db_dbname"), host=Sys.getenv("mlb_db_hostname"), port = 5432, user = Sys.getenv("mlb_db_username"), password = Sys.getenv("mlb_db_password")) 
+my_db <- src_mysql(dbname = Sys.getenv("mlb_db_dbname"), host=Sys.getenv("mlb_db_hostname"), port = 3306, user = Sys.getenv("mlb_db_username"), password = Sys.getenv("mlb_db_password")) 
 scrape(start = "2018-04-02", end = "2018-04-08", suffix = "inning/inning_all.xml", connect = my_db$con)
 
 #sql_command <- paste("UPDATE pg_settings SET setting =", Sys.Date(), "WHERE name = 'latest_date'")
