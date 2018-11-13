698a96801763e060bfd714bc6ea09c26eec153cb
Jason R. Battles <jason.r.battles@gmail.com>
2018-11-13 13:42:56 -0600

    test memory handling

--- 00-mlb-datafresh.R
+++ 00-mlb-datafresh.R
@@ -10,6 +10,13 @@ names(s <- Sys.getenv())
 #Establish data connection using PAS System Environment Variables
 my_db <- src_postgres(dbname = Sys.getenv("mlb_db_dbname"), host=Sys.getenv("mlb_db_hostname"), port = 5432, user = Sys.getenv("mlb_db_username"), password = Sys.getenv("mlb_db_password")) 
 
+week1DF <- scrape(start = "2018-03-29", end = "2016-04-07", suffix = "inning/inning_all.xml", connect = my_db$con)
+rm(week1DF)
+week2DF <- scrape(start = "2018-04-08", end = "2016-04-14", suffix = "inning/inning_all.xml", connect = my_db$con)
+rm(week2DF)
+week3DF <- scrape(start = "2018-04-15", end = "2016-04-21", suffix = "inning/inning_all.xml", connect = my_db$con)
+rm(week3DF)
+
 #sql_command <- paste("UPDATE pg_settings SET setting =", Sys.Date(), "WHERE name = 'latest_date'")
 #dbGetQuery(con, sql_command)
 
@@ -18,69 +25,71 @@ my_db <- src_postgres(dbname = Sys.getenv("mlb_db_dbname"), host=Sys.getenv("mlb
 
 #print(nrow(atbat_tbl))
 
-Today <- Sys.Date()
-TodayYearDay = as.POSIXlt(c(Today))$yday  #Year Day for Today
-#Today <- as.Date("2018-06-10")            # for testing
-#TodayYearDay = as.POSIXlt(c(Today))$yday  # for testing
-SeasonStartYearDay = as.POSIXlt(c(as.Date("2018-03-29")))$yday
-SeasonEndYearDay = as.POSIXlt(c(as.Date("2018-09-30")))$yday
-PostSeasonEndYearDay = as.POSIXlt(c(as.Date("2018-10-31")))$yday
-
-#pick startdate and end date
-#March
-if (TodayYearDay <= PostSeasonEndYearDay) {  # is it before the end of the Post season
-    if (TodayYearDay > SeasonEndYearDay) { # well, it Is Post Season
-        print("It is Post Season - grab 90 days of data and Post Season data")
-        startdate = Today - 90 - (TodayYearDay - SeasonEndYearDay) - 1
-        enddate = Today
-        splitSeasonsPreviousDays = 0
-    } else if (TodayYearDay >= SeasonStartYearDay + 90 ) { # Regular Season
-        print("It is Mid Season - grab 90 days of data")
-        startdate = Today - 90
-        enddate = Today
-        splitSeasonsPreviousDays = 0
-    } else if (TodayYearDay <= SeasonStartYearDay) {       # Before Regular Season
-        print("Use Last Years data")
-        enddate = Today - TodayYearDay - (365 - PostSeasonEndYearDay)
-        startdate = Today - TodayYearDay - (365 - PostSeasonEndYearDay) - 120
-        splitSeasonsPreviousDays = 0
-    } else {
-        print("It is Mid Season, but it is too early to get a full 90 days this year")
-        startdate = Today - (TodayYearDay - SeasonStartYearDay)
-        enddate = Today
-        splitSeasonsPreviousDays = 120 - (TodayYearDay - SeasonStartYearDay) 
-    }
-    
-} else  {         # it is After Post Season Or Later at the end of the year
-    print("It is After post season toward the end of the year")
-    startdate = Today - (TodayYearDay - PostSeasonEndYearDay) - 120
-    enddate = Today - (TodayYearDay - PostSeasonEndYearDay)
-    splitSeasonsPreviousDays = 0
-} 
-
-message(sprintf("Today: %s", Today))
-message(sprintf("Start Date: %s", startdate))
-message(sprintf("End Date: %s", enddate))
-message(sprintf("currnet season days: %s", enddate - startdate))
-message(sprintf("Split season previous season days: %s\n", splitSeasonsPreviousDays))
-
-#Pull Current Season Days
-scrape(start = startdate, enddate, suffix = "inning/inning_all.xml", connect = my_db$con)
-
-#Pull any Split Season Days
-if (splitSeasonsPreviousDays > 0) {
-    previousSeasonStartdate = Today - TodayYearDay - (365 - PostSeasonEndYearDay) - splitSeasonsPreviousDays
-    PreviousSeasonEnddate = Today - TodayYearDay - (365 - PostSeasonEndYearDay)
-    scrape(start = startdate, enddate, suffix = "inning/inning_all.xml", connect = my_db$con)
-    message(sprintf("Split Season Start Date: %s", previousSeasonStartdate))
-    message(sprintf("Split Season End Date: %s", PreviousSeasonEnddate))
-    message(sprintf("Split Season days: %s", PreviousSeasonEnddate - previousSeasonStartdate))
-}
-
-dbSendQuery(my_db$con, "CREATE INDEX url_atbat ON atbat(url)") 
-dbSendQuery(my_db$con, "CREATE INDEX url_pitch ON pitch(url)")
-dbSendQuery(my_db$con, "CREATE INDEX pitcher_index ON atbat(pitcher_name)")
-dbSendQuery(my_db$con, "CREATE INDEX des_index ON pitch(des)")
+##### Block Comment
+#####
+# Today <- Sys.Date()
+# TodayYearDay = as.POSIXlt(c(Today))$yday  #Year Day for Today
+# #Today <- as.Date("2018-06-10")            # for testing
+# #TodayYearDay = as.POSIXlt(c(Today))$yday  # for testing
+# SeasonStartYearDay = as.POSIXlt(c(as.Date("2018-03-29")))$yday
+# SeasonEndYearDay = as.POSIXlt(c(as.Date("2018-09-30")))$yday
+# PostSeasonEndYearDay = as.POSIXlt(c(as.Date("2018-10-31")))$yday
+# 
+# #pick startdate and end date
+# #March
+# if (TodayYearDay <= PostSeasonEndYearDay) {  # is it before the end of the Post season
+#     if (TodayYearDay > SeasonEndYearDay) { # well, it Is Post Season
+#         print("It is Post Season - grab 90 days of data and Post Season data")
+#         startdate = Today - 90 - (TodayYearDay - SeasonEndYearDay) - 1
+#         enddate = Today
+#         splitSeasonsPreviousDays = 0
+#     } else if (TodayYearDay >= SeasonStartYearDay + 90 ) { # Regular Season
+#         print("It is Mid Season - grab 90 days of data")
+#         startdate = Today - 90
+#         enddate = Today
+#         splitSeasonsPreviousDays = 0
+#     } else if (TodayYearDay <= SeasonStartYearDay) {       # Before Regular Season
+#         print("Use Last Years data")
+#         enddate = Today - TodayYearDay - (365 - PostSeasonEndYearDay)
+#         startdate = Today - TodayYearDay - (365 - PostSeasonEndYearDay) - 120
+#         splitSeasonsPreviousDays = 0
+#     } else {
+#         print("It is Mid Season, but it is too early to get a full 90 days this year")
+#         startdate = Today - (TodayYearDay - SeasonStartYearDay)
+#         enddate = Today
+#         splitSeasonsPreviousDays = 120 - (TodayYearDay - SeasonStartYearDay) 
+#     }
+#     
+# } else  {         # it is After Post Season Or Later at the end of the year
+#     print("It is After post season toward the end of the year")
+#     startdate = Today - (TodayYearDay - PostSeasonEndYearDay) - 120
+#     enddate = Today - (TodayYearDay - PostSeasonEndYearDay)
+#     splitSeasonsPreviousDays = 0
+# } 
+# 
+# message(sprintf("Today: %s", Today))
+# message(sprintf("Start Date: %s", startdate))
+# message(sprintf("End Date: %s", enddate))
+# message(sprintf("currnet season days: %s", enddate - startdate))
+# message(sprintf("Split season previous season days: %s\n", splitSeasonsPreviousDays))
+# 
+# #Pull Current Season Days
+# scrape(start = startdate, enddate, suffix = "inning/inning_all.xml", connect = my_db$con)
+# 
+# #Pull any Split Season Days
+# if (splitSeasonsPreviousDays > 0) {
+#     previousSeasonStartdate = Today - TodayYearDay - (365 - PostSeasonEndYearDay) - splitSeasonsPreviousDays
+#     PreviousSeasonEnddate = Today - TodayYearDay - (365 - PostSeasonEndYearDay)
+#     scrape(start = startdate, enddate, suffix = "inning/inning_all.xml", connect = my_db$con)
+#     message(sprintf("Split Season Start Date: %s", previousSeasonStartdate))
+#     message(sprintf("Split Season End Date: %s", PreviousSeasonEnddate))
+#     message(sprintf("Split Season days: %s", PreviousSeasonEnddate - previousSeasonStartdate))
+# }
+# 
+# dbSendQuery(my_db$con, "CREATE INDEX url_atbat ON atbat(url)") 
+# dbSendQuery(my_db$con, "CREATE INDEX url_pitch ON pitch(url)")
+# dbSendQuery(my_db$con, "CREATE INDEX pitcher_index ON atbat(pitcher_name)")
+# dbSendQuery(my_db$con, "CREATE INDEX des_index ON pitch(des)")
 
 ### Load pitch and atbat data frames
 pitchesDF <- select(tbl(my_db, "pitch"), gameday_link, num, des, type, tfs, tfs_zulu, id, end_speed, pitch_type, count, zone)
