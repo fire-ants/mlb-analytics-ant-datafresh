64e327d6821d696880b113d75613ff6f42f72218
Cohen <dan.cohen@emc.com>
2018-11-17 10:37:06 -0700

    added param and implemented scarpe db before appending to longterm mlb database

--- 00-mlb-datafresh.R
+++ 00-mlb-datafresh.R
@@ -12,11 +12,12 @@ cat("R program running")
 
 cat("R program running: opening connections to DB")
 
-#connection used by pitchrx scrape function via dbply/dplyr
-my_pitchrx_db <- src_mysql(dbname = Sys.getenv("mlb_db_dbname"), host = Sys.getenv("mlb_db_hostname"), port = 3306, user = Sys.getenv("mlb_db_username"), password = Sys.getenv("mlb_db_password"))
+#connection used by pitchrx scrape function to aquire data from MLB for this data pull and process job via dbply/dplyr
+#my_scrape_db <- src_mysql(dbname = Sys.getenv("mlb_db_scrape"), host = Sys.getenv("mlb_db_hostname"), port = 3306, user = Sys.getenv("mlb_db_username"), password = Sys.getenv("mlb_db_password"))
 
-# connection used by DBI::dbWriteTable() to write new dataframe to MYSQL database
-my_dbi_db <- DBI::dbConnect(RMySQL::MySQL(), 
+#connection used by datfresh to store processed MLB data long term - via DBI::dbWriteTable()
+#connection used by DBI::dbWriteTable() to write new dataframe to MYSQL database
+my_mlb_db <- DBI::dbConnect(RMySQL::MySQL(), 
                       host = Sys.getenv("mlb_db_hostname"),
                       dbname = Sys.getenv("mlb_db_dbname"),
                       user = Sys.getenv("mlb_db_username"),
@@ -36,23 +37,28 @@ my_dbi_db <- DBI::dbConnect(RMySQL::MySQL(),
 # end today, maybe
 # end = Sys.Date()
 
-start <- as.Date("03-29-18",format="%m-%d-%y")
-end   <- as.Date("04-08-18",format="%m-%d-%y")
+start <- as.Date("04-09-18",format="%m-%d-%y")
+end <- start + 13
+#end   <- as.Date("04-22-18",format="%m-%d-%y")
 
 dates <- seq(start, end, by="day")
 
 for(di in as.list(dates)) {
-  my_pitchrx_db <- src_mysql(dbname = Sys.getenv("mlb_db_dbname"), host = Sys.getenv("mlb_db_hostname"), port = 3306, user = Sys.getenv("mlb_db_username"), password = Sys.getenv("mlb_db_password"))
-  try(scrape(start = format(di,"%Y-%m-%d"), end = format(di,"%Y-%m-%d"), suffix = "inning/inning_all.xml", connect = my_pitchrx_db$con))
-  rm(my_db)
+  #connection used by pitchrx scrape function via dbply/dplyr
+  my_scrape_db <- src_mysql(dbname = Sys.getenv("mlb_db_scrape"), host = Sys.getenv("mlb_db_hostname"), port = 3306, user = Sys.getenv("mlb_db_username"), password = Sys.getenv("mlb_db_password"))
+  #try (if there are games) to scrape for this day using the scrape connection and write into the scrape db
+  try(scrape(start = format(di,"%Y-%m-%d"), end = format(di,"%Y-%m-%d"), suffix = "inning/inning_all.xml", connect = my_scrape_db$con))
+  # clean up the scrape connection 
+  rm(my_scrape_db)
+  # repeat for all days in range
 }
 
 
 cat("R program running: pulling pitch and atbat dataframes from database")
 
 ### Load pitch and atbat data frames
-pitchesDF <- select(tbl(my_pitchrx_db, "pitch"), gameday_link, num, des, type, tfs, tfs_zulu, id, end_speed, pitch_type, count, zone)
-atbatsDF <- select(tbl(my_pitchrx_db, "atbat"), gameday_link, num, pitcher, batter, b_height, pitcher_name, p_throws, batter_name, stand, atbat_des, event, inning, inning_side)
+pitchesDF <- select(tbl(my_scrape_db, "pitch"), gameday_link, num, des, type, tfs, tfs_zulu, id, end_speed, pitch_type, count, zone)
+atbatsDF <- select(tbl(my_scrape_db, "atbat"), gameday_link, num, pitcher, batter, b_height, pitcher_name, p_throws, batter_name, stand, atbat_des, event, inning, inning_side)
 
 get_quant_score <- function(des) {
     score <- (
@@ -171,14 +177,6 @@ var.interest <- joined.classic.pitchedit %>% select(3,5,6,8:13,16,18,22,27:29)
 
 cat("R program running: storing results in database")
 
-DBI::dbWriteTable(my_dbi_db, "rawdata_joined", joined.classic.pitchedit, overwrite = TRUE)
-DBI::dbWriteTable(my_dbi_db, "rawdata_ML", var.interest, overwrite = TRUE)
-dbDisconnect(my_dbi_db)
-
-#sql_command <- paste("UPDATE pg_settings SET setting =", Sys.Date(), "WHERE name = 'latest_date'")
-#dbGetQuery(con, sql_command)
-
-#atbat_tbl <- tbl(my_db, "atbat")
-#pitch_tbl <- tbl(my_db, "pitch")
-
-#print(nrow(atbat_tbl))
+DBI::dbWriteTable(my_mlb_db, "rawdata_joined", joined.classic.pitchedit)
+DBI::dbWriteTable(my_mlb_db, "rawdata_ML", var.interest)
+dbDisconnect(my_mlb_db)
